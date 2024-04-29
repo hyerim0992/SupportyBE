fa33f75ad9b612d4e8d08a67b2abe344db657704
hyerim <parkhyerim10@gmail.com>
2024-04-29 05:57:59 +0000

    modified:   routes/drugsRouter.js

--- routes/drugsRouter.js
+++ routes/drugsRouter.js
@@ -1,32 +1,38 @@
 "use strict";
 
-const express = require('express')
-const router = express.Router()
-const drugsController = require("../controllers/drugsController")
-const methodOverride = require("method-override")
+const express = require('express');
+const router = express.Router();
+const drugsController = require("../controllers/drugsController");
+const methodOverride = require("method-override");
 
 router.use(
     methodOverride("_method", {
         methods: ["POST", "GET"]
     })
-    );
-    
-    router.use(
+);
+
+router.use(
     express.urlencoded({
         extended: false
     })
-    );
-    router.use(express.json());
-      
+);
+router.use(express.json());
 
-router.get("/", (req,res)=> {
+
+router.get("/", (req, res) => {
     res.send("복용약 page home");
-  });
-  router.get("/today",drugsController.showTodayDrugList);
-  router.get("/entire", drugsController.showEntireDrugList);
-  router.get("/today/record",drugsController.showTodayDrugRecord);
-  router.get("/entire/record",drugsController.showDrugRecord);
-  router.get("/info",drugsController.showDrugInfo);
+});
 
-  module.exports = router;
-  
+router.route("/today")
+    .get(drugsController.showTodayDrugList)
+    .post()
+    .put()
+    .delete();
+
+router.route("/entire")
+
+router.get("/today/record", drugsController.showTodayDrugRecord);
+router.get("/entire/record", drugsController.showDrugRecord);
+router.get("/info", drugsController.showDrugInfo);
+
+module.exports = router;
